9391f55fcf5fc9635256bed9dabe5302dc645f2c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-08 20:35:57 +0300

    feat: ajouter la route d'enregistrement pour l'authentification

--- server/src/routes/public/auth.route.ts
+++ server/src/routes/public/auth.route.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono"
+import { registerController } from "../../controllers/auth.controller"
 
 const authRoutes = new Hono()
+  .post("/register", registerController)
 
 export default authRoutes
