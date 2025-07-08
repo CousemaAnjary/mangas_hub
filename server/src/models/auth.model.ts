9c2e7eb2d419beb08ec090fcec8e0602d4c00937
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-08 19:47:57 +0300

    fix: ajouter une valeur par défaut pour la colonne createdAt dans le modèle d'utilisateur

--- server/src/models/auth.model.ts
+++ server/src/models/auth.model.ts
@@ -7,8 +7,7 @@ export const users = pgTable("users", {
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
   image: text("image"),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(), 
 })
 
 // Type générés automatiquement par Drizzle
