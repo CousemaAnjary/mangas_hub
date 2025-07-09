2c3f5612b1ad13fd45afe19b2f4ee75992276ddb
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-09 20:29:24 +0300

    feat: implémenter la fonction de connexion utilisateur et ajouter le type LoginResponse

--- client/src/modules/auth/services/auth.service.ts
+++ client/src/modules/auth/services/auth.service.ts
@@ -1,6 +1,16 @@
+import z from "zod"
+import { LoginResponse } from "../types/auth"
+import { authFetch } from "@/src/utils/authFetch"
+import { loginSchema } from "@/src/validations/auth.validation"
+
 
 // Fonction pour connecter un utilisateur
-export const loginUser = async () => {}
+export const loginUser = async (data: z.infer<typeof loginSchema>) => {
+  return await authFetch<LoginResponse>("/auth/login", {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+}
 
 // Fonction pour déconnecter un utilisateur
 export const logoutUser = async () => {}
