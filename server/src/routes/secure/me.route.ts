37f354bb7c11736e5524e1fc56ab8072606dfd7c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-09 23:06:48 +0300

    feat: ajouter la route pour récupérer les informations de l'utilisateur actuel

--- server/src/routes/secure/me.route.ts
+++ server/src/routes/secure/me.route.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono"
+import { getCurrentUserController } from "../../controllers/me.controller"
 
 const meRoutes = new Hono()
+  .get("/", getCurrentUserController)
 
 export default meRoutes
