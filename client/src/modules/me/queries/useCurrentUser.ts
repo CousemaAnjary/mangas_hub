7d9b90ac6067e2be16b8fc7761a88398a56c0911
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-09 23:30:58 +0300

    feat: ajouter la requête pour récupérer les informations de l'utilisateur actuel

---
+++ client/src/modules/me/queries/useCurrentUser.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query"
+import { getCurrentUser } from "../services/me.service"
+
+export const useCurrentUser = () => {
+  return useQuery({
+    // Clé unique pour cette requête
+    queryKey: ["current-user"],
+    queryFn: getCurrentUser,
+  })
+}
--- client/src/modules/me/services/me.service.ts
+++ client/src/modules/me/services/me.service.ts
@@ -1,7 +1,7 @@
 import { authFetch } from "@/src/utils/authFetch"
 import { GetCurrentUserResponse } from "../types/me"
 
-
+// Récupérer les informations de l'utilisateur actuel
 export const getCurrentUser = async () => {
    const res = await authFetch<GetCurrentUserResponse>("/me")
    return res.payload
