8dd46e04282b50790c1b08735b84bb7746c3cea2
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-09 23:14:39 +0300

    fix: corriger l'attente du service pour récupérer les informations de l'utilisateur actuel

--- server/src/controllers/me.controller.ts
+++ server/src/controllers/me.controller.ts
@@ -5,7 +5,7 @@ import { jsonError } from "../utils/jsonError"
 // Récupérer les informations de l'utilisateur actuel
 export const getCurrentUserController = async (c: Context) => {
   try {
-    const payload = getCurrentUserService(c)
+    const payload = await getCurrentUserService(c)
     return c.json({ success: true, message: "Utilisateur récupéré avec succès", payload }, 200)
 
   } catch (error) {
