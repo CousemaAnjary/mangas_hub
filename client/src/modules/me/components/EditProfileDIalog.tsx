b6bde58b99d693eccce83b54389c2f715775fa94
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-10 21:11:42 +0300

    feat: ajouter le composant EditProfileDialog avec la structure de base

---
+++ client/src/modules/me/components/EditProfileDIalog.tsx
@@ -0,0 +1,19 @@
+export default function EditProfileDIalog() {
+  /**
+   * ! STATE (état, données) de l'application
+   */
+  
+  
+  /**
+   * ! COMPORTEMENT (méthodes, fonctions) de l'application
+   */
+  
+  
+  /**
+   * ! AFFICHAGE (render) de l'application
+   */
+  return (
+    <>
+    </>
+  )
+}
