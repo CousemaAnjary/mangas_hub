a0622e2b01b2ce30ed707a585c4309103efec6a5
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-10 19:22:15 +0300

    fix: mettre à jour la gestion du succès dans le hook useLogout pour effacer le cache après déconnexion

--- client/src/modules/auth/hooks/useLogout.ts
+++ client/src/modules/auth/hooks/useLogout.ts
@@ -1,6 +1,7 @@
 import { toast } from "sonner"
 import { useMutation } from "@tanstack/react-query"
 import { logoutUser } from "../services/auth.service"
+import { queryClient } from "@/src/lib/react-query"
 
 
 export const useLogout = () => {
@@ -11,6 +12,6 @@ export const useLogout = () => {
 
     // Gestion des erreurs et success
     onError: (error) => { toast.error(error.message) },
-    onSuccess: (response) => {toast.success(response.message)},
+    onSuccess: () => { queryClient.clear() }
   })
 }
