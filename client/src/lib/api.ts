6137280cca5e442f987f183e89b3161b331373d6
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-12 19:37:38 +0300

    fix: ajouter la gestion de l'URL des uploads dans les variables d'environnement

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,3 +1,6 @@
 export const apiUrl = process.env.NEXT_PUBLIC_API_URL 
 if (!apiUrl) throw new Error("NEXT_PUBLIC_API_URL is not defined in environment variables")
 
+
+export const uploadsUrl = process.env.NEXT_PUBLIC_UPLOADS_URL
+if (!uploadsUrl) throw new Error("NEXT_PUBLIC_UPLOADS_URL is not defined in environment variables")
