240ad3c07047c6c9d09be5f289cd58c3cabcf184
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-12 17:12:33 +0300

    fix: supprimer un espace inutile dans le contrôleur de mise à jour de l'utilisateur

--- server/src/controllers/me.controller.ts
+++ server/src/controllers/me.controller.ts
@@ -14,10 +14,11 @@ export const getCurrentUserController = async (c: Context) => {
   }
 }
 
+// Mettre à jour les informations de l'utilisateur
 export const updateUserController = async (c: Context) => {
   try {
     return c.json({ success: true, message: "Utilisateur mis à jour avec succès" }, 200)
-    
+
   }catch (error) {
     return c.json(jsonError(error), 500)
   }
