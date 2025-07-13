bda4e0ed6abc46bc12b375be7944c8272bb4ce4e
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-13 22:59:28 +0300

    feat: ajouter la gestion des images avec le composant Image de Next.js dans AvatarProfile

--- client/src/modules/me/components/AvatarProfile.tsx
+++ client/src/modules/me/components/AvatarProfile.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable @next/next/no-img-element */
 "use client"
 
+import Image from "next/image"
 import { XIcon } from "lucide-react"
 import { backendUrl } from "@/src/lib/api"
 import user from "@/public/images/user.png"
 import { Button } from "@/src/components/ui/button"
 import { useCurrentUser } from "../queries/useCurrentUser"
-
 
 
 export default function AvatarProfile() {
@@ -25,18 +24,28 @@ export default function AvatarProfile() {
    */
   return (
     <div className="relative size-36">
-      <Button
+         <Button
         variant="outline"
-        className="group relative size-full rounded-xl border-2 border-white p-0 shadow-md overflow-hidden bg-transparent hover:bg-transparen cursor-default"
+        className="group relative size-full rounded-xl border-2 border-white p-0 shadow-md overflow-hidden bg-transparent hover:bg-transparent cursor-default"
       >
         <div className="absolute inset-0">
-          <img
-            src={previewUrl ?? user.src}
-            alt="Image de profil"
-            className={`h-full w-full transition-all duration-200 ${
-              previewUrl || currentUser?.image ? "object-cover" : "object-contain p-5"
-            }`}
-          />
+          {previewUrl ? (
+            <Image
+              src={previewUrl}
+              alt="Image de profil"
+              fill
+              className="object-cover"
+              sizes="144px"
+            />
+          ) : (
+            <Image
+              src={user}
+              alt="Image de profil par défaut"
+              fill
+              className="object-contain p-5"
+              sizes="144px"
+            />
+          )}
         </div>
       </Button>
 
