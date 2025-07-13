139e19e03bf66901e6f5325e112ab2b77098be6e
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-13 16:27:55 +0300

    fix: remplacer l'importation de queryClient par l'initialisation de QueryClient dans TanstackProvider

--- client/src/providers/tanstack-provider.tsx
+++ client/src/providers/tanstack-provider.tsx
@@ -1,9 +1,14 @@
 "use client"
 
-import { queryClient } from "../lib/react-query"
-import { QueryClientProvider } from "@tanstack/react-query"
+import { useState } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 
 export const TanstackProvider = ({ children }: { children: React.ReactNode }) => {
-   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+   const [queryClient] = useState(() => new QueryClient())
+   return (
+      <QueryClientProvider client={queryClient}>
+         {children}
+      </QueryClientProvider>
+   )
 }
